9f19a1c8f579e7c25fccb17dda5f173d819f4783
Nathan Schrader <natecschrader@gmail.com>
2021-06-28 13:45:53 -0700

    add Sidebar component to mirror Navbar links

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { 
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from './SidebarElements';
+
+
+const Sidebar = () => {
+  return (
+    <>
+      <SidebarContainer>
+        <Icon>
+          <CloseIcon />
+        </Icon>
+          <SidebarWrapper>
+            <SidebarMenu>
+              <SidebarLink to="about">
+                About
+              </SidebarLink>
+              <SidebarLink to="portfolio">
+                Portfolio
+              </SidebarLink>
+              <SidebarLink to="contact">
+                Contact Me
+              </SidebarLink>
+            </SidebarMenu>
+            <SideBtnWrap>
+              <SidebarRoute to="/signin">Sign In</SidebarRoute>
+            </SideBtnWrap>
+          </SidebarWrapper>
+      </SidebarContainer>
+    </>
+  )
+}
+
+export default Sidebar;
