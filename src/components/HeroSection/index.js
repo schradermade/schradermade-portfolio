b2371678c0d76ef19dd0772762f7c88f7ec2f773
Nathan Schrader <natecschrader@gmail.com>
2021-06-29 16:20:41 -0700

    add HeroSection component with looping video

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Video from '../../videos/video.mp4';
+import { 
+  HeroContainer,
+  HeroBg,
+  VideoBg 
+  } from './HeroElement';
+
+const HeroSection = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection;
