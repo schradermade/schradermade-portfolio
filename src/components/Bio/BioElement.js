54f1dfbe6d5b3437c2ffa9330c69450f985f3bb5
Nathan Schrader <natecschrader@gmail.com>
2021-06-30 16:22:28 -0700

    wrapped Bio copy in custom component to change color on hover

--- src/components/Bio/BioElement.js
+++ src/components/Bio/BioElement.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
 export const BioContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
   
+`
+
+export const BioHover = styled.p`
+  color: #000;
+
+  &:hover {
+    color: #fff;
+  }
+`
+
+export const BioInvis = styled.p`
+  opacity: 0%;
 `
--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
   BioContainer,
+  BioHover,
+  BioInvis,
 }
 from './BioElement';
 
@@ -10,7 +12,33 @@ const Bio = () => {
   return (
     <>
       <BioContainer>
-        Hi, I'm Nathan, a front end engineer specializing in ReactJS web and mobile applications. 
+        <BioHover>Hi,</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>I'm</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>Nathan,</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>a</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>front</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>end</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>engineer</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>specializing</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>in</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>ReactJS</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>web</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>and</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>mobile</BioHover>
+        <BioInvis>.</BioInvis>
+        <BioHover>applications.</BioHover>
       </BioContainer>
     </>
   )
